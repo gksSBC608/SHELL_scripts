0aae0643fd3b57e418d8cd0b480914ee5376c64c
gksSBC608 <gksSBC608@users.noreply.github.com>
2020-05-19 08:47:14 +0530

    Updated the script to take thread dump

--- thread_dump.sh
+++ thread_dump.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-echo "hello world"
+# This SHELL script takes thread dump specific number of times as passed as argument to it                                                                            ment
+# First parameter is the PID of a java process
+# Second parameter is the number of times, the thread dump should be taken
+# Third argument is the time interval in seconds between two thread dumps
+#pid=$1
+set -x
+pid=$(ps aux | grep java| grep -v grep | awk '{print $2}')
+COUNT=1
+MAX_COUNT=$2
+TIME_INTERVAL=$3
+while [ ${COUNT} -le ${MAX_COUNT} ]
+do
+#echo "index is ${COUNT}"
+#echo
+jstack -l ${pid} > dumps/thread_dump_${COUNT}.log
+sleep ${TIME_INTERVAL};
+COUNT=$(( $COUNT+1))
+echo
+#echo  "Index updated to $COUNT"
+#echo
+done
